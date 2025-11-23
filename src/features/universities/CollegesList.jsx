7c7f833a13dec6434f3e8a2228bf5cec4f2c9f23
Chiranjivi Basnet <basnetchiran1010@gmail.com>
2025-11-23 15:23:05 +0545

    build universities/colleges page

---
+++ src/features/universities/CollegesList.jsx
@@ -0,0 +1,88 @@
+function CollegesList() {
+  const Colleges = [
+    {
+      name: "Medicine - MBBS, PG",
+      location: "Bos, Geo, Rus, Latvia, Uzbek",
+      link: "https://docs.google.com/spreadsheets/d/1gTzTzDr5B6V0D6Rw5z1A9Rgfv4F54YeH/edit?pli=1&gid=273337086#gid=273337086",
+    },
+    {
+      name: "All The Universities of Malta",
+      location: "Malta",
+      link: "https://docs.google.com/spreadsheets/d/190nWxtj5bYIloVAWvEtsykB70mcgd4XL/edit?gid=898970686#gid=898970686",
+    },
+    {
+      name: "Universities of Malta (UM) Courses",
+      location: "Malta",
+      link: "https://docs.google.com/spreadsheets/d/1fMuG6ILgmVdB6HP9cLN27BvyFCIa4lxa/edit?gid=1587582161#gid=1587582161",
+    },
+    {
+      name: "All the Universities of Latvia",
+      location: "Latvia",
+      link: "https://docs.google.com/spreadsheets/d/145arRI1RSDgpR4wJGRd5ZXu2Yspura3-/edit?gid=15066516#gid=15066516",
+    },
+    {
+      name: "All the Universities of Romania",
+      location: "Romania",
+      link: "https://docs.google.com/spreadsheets/d/1d6rFJwgKv4HwhrbGzR-odJlOmQ68XEVy/edit?gid=826976884#gid=826976884",
+    },
+    {
+      name: "All the Universities of United Kingdom (UK)",
+      location: "United Kingdom (UK)",
+      link: "https://docs.google.com/spreadsheets/d/1kMx85MCQwNlfLH3CwzAwzutUENoEsV1S/edit?gid=779513140#gid=779513140",
+    },
+  ];
+
+  return (
+    <section className=" py-10 sm:py-16 lg:py-20 bg-[var(--color-secondary-50)]">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-10">
+        <h3 className="text-[var(--color-primary-500)] font-semibold text-2xl sm:text-3xl md:text-4xl text-center mb-6 md:mb-9">
+          List of colleges of European countries is given below
+        </h3>
+
+        <div className="overflow-x-auto shadow-lg mx-auto max-w-4xl rounded-2xl">
+          <table className="w-full ">
+            <thead className="text-lg sm:text-xl bg-[var(--color-primary-500)] text-[var(--color-primary-50)]">
+              <tr className="">
+                <th className="cell">S.N.</th>
+                <th className="cell break-all text-left">
+                  University/College Name
+                </th>
+                <th className="cell text-left">Location</th>
+                <th className="cell">More Details</th>
+              </tr>
+            </thead>
+
+            <tbody className="p-15">
+              {Colleges.map((college, i) => {
+                return (
+                  <tr className="even:bg-[var(--color-secondary-100)]">
+                    <td className="cell text-center">{i + 1}</td>
+                    <td className=" cell">{college.name}</td>
+                    <td className="cell ">{college.location}</td>
+                    <td className="cell text-center">
+                      {" "}
+                      <a
+                        target="_blank"
+                        href={college.link}
+                        className="text-[var(--color-primary-600)] font-semibold hover:underline"
+                      >
+                        Click here
+                      </a>{" "}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+
+        <p className="text-center text-gray-500 text-sm mt-6">
+          Click on "Click here" to view detailed information about each
+          institution
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default CollegesList;
--- src/pages/Universities.jsx
+++ src/pages/Universities.jsx
@@ -1,5 +1,21 @@
-function Universities() {
-  return <div>universities and colleges</div>;
-}
+import CollegesList from "../features/universities/CollegesList";
+import Footer from "../ui//Footer";
 
+function Universities() {
+  return (
+    <>
+      <section className="bg-[var(--color-primary-500)] text-[var(--color-primary-50)]">
+        <div className="py-18">
+          <h1 className="text-4xl sm:text-5xl text-center font-bold">
+            Universities / Colleges
+          </h1>
+        </div>
+      </section>
+
+      <CollegesList />
+
+      <Footer />
+    </>
+  );
+}
 export default Universities;
